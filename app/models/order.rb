71576bb6d702965a2cfacd1e50be1892429a1832
Tom Brice <tomtoday@gmail.com>
2014-04-08 17:06:45 -0500

    Make sure completed_on is in the past

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,15 @@
 class Order < ActiveRecord::Base
   validates :customer_name, :customer_email, :description, :price, presence: true
+  validate :completion_date_must_be_in_the_past
 
   def self.unpaid
     where(paid_for_on: nil)
   end
+
+  private
+
+  def completion_date_must_be_in_the_past
+    errors.add(:completed_on, 'should not be in the future.') if completed_on && completed_on > Time.now
+  end
+
 end
