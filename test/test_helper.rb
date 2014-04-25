cc76f18ed35775504ceb24d3a63a27074aab0a45
Joe Slag <joe@slagwerks.com>
2014-04-25 11:22:22 -0500

    remaining 4.1 related upgrades

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,3 @@
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-
-class ActiveSupport::TestCase
-  ActiveRecord::Migration.check_pending!
-
-  # Add more helper methods to be used by all tests here...
-end
