64a11c044921daf4bc120d2cb76cbaec9f4a8cbf
Joe Slag <joe@slagwerks.com>
2014-04-28 15:27:28 -0500

    tests checking if dates can be directly updated

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -56,4 +56,36 @@ class OrdersControllerTest < ActionController::TestCase
 
     assert_redirected_to orders_path
   end
+
+  test "should mark order paid" do
+    assert_nil @order.paid_for_on
+    post :mark_paid, id: @order
+
+    @order.reload
+    assert_not_nil @order.paid_for_on
+  end
+
+  test "should mark order completed" do
+    assert_nil @order.completed_on
+    post :mark_completed, id: @order
+
+    @order.reload
+    assert_not_nil @order.completed_on
+  end
+
+  test "should not allow directly setting paid_for_on" do
+    assert_date_field_not_updatable :paid_for_on
+  end
+
+  test "should not allow directly setting completed_on" do
+    assert_date_field_not_updatable :completed_on
+  end
+
+  def assert_date_field_not_updatable(field)
+    assert_nil @order.send(field)  # Sanity check: this test assumes date field is nil
+    patch :update, id: @order, order: { field => Time.now }
+
+    @order.reload
+    assert_nil @order.send(field), "Client directly updated #{field}"
+  end
 end
